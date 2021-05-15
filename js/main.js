3dc56e30be2663d6f54f8170d679fa0f2ba68980
Yasuhiro WATANABE［渡邊泰宏］ <82741210+yasuhiro112358@users.noreply.github.com>
2021-05-15 20:13:10 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+{
+    const overlay = document.querySelector('.overlay');
+    const overlayMain = document.querySelector('.overlay-main');
+
+
+    const open = document.getElementById('open');
+
+    open.addEventListener('click', () => {
+        overlay.classList.add('show');
+        overlayMain.classList.add('show');
+        open.classList.add('hide');
+    });
+    
+    
+    const close = document.getElementById('close');
+    
+    close.addEventListener('click', () => {
+        overlay.classList.remove('show');
+        overlayMain.classList.remove('show');
+        open.classList.remove('hide');
+    });
+    
+
+}
